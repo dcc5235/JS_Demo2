88adc526611481a50bbe35fdd0007565ae79a951
Dany <dany@Danys-MacBook-Pro.local>
2020-09-30 11:56:10 -0500

    added event listener and function

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
+todoButton.addEventListener("click", addTodo);
 
 //Functions
+
+function addTodo(event) {
+  //prevent form from submitting
+  event.preventDefault();
+}
